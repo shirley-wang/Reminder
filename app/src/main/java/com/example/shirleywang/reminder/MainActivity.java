2b0b3517e9c54c89f5b023d1ec9bb9aeb890ce0e
shirley-wang <xuanli512@gmail.com>
2016-04-01 01:13:41 -0500

    popup window updated

--- app/src/main/java/com/example/shirleywang/reminder/MainActivity.java
+++ app/src/main/java/com/example/shirleywang/reminder/MainActivity.java
@@ -19,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // todobutton call adding new task popup
         Button todoButton = (Button)findViewById(R.id.todo_button);
         todoButton.setOnClickListener(
                 new Button.OnClickListener(){
--- app/src/main/java/com/example/shirleywang/reminder/TodoList.java
+++ app/src/main/java/com/example/shirleywang/reminder/TodoList.java
@@ -1,9 +1,13 @@
 package com.example.shirleywang.reminder;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.*;
 
 public class TodoList extends AppCompatActivity {
 
@@ -11,7 +15,18 @@ public class TodoList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo_list);
+
+        Button addTaskButton = (Button)findViewById(R.id.new_task_button);
+        addTaskButton.setOnClickListener(
+                new Button.OnClickListener(){
+                    public void onClick(View v){
+                        Intent i = new Intent(TodoList.this,popupWindow.class);
+                        startActivity(i);
+                    }
+                }
+        );
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
